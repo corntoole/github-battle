00e30e39bbe44c2e79c2aa1d6597295e4474df52
Cornelius Toole <cornelius.toole@gmail.com>
2016-04-23 14:16:55 -0500

    gh-battle now GETs player info, dumps its onscreen
      - added axios dependency
      - added util to retrieve gh profile info

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,9 +1,25 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
+function puke (object) {
+  return <pre>{JSON.stringify(object, null, ' ')}</pre>
+}
+function ConfirmBattle (props) {
+  return props.isLoading === true
+    ? <p>LOADING</p>
+  : <div>CONFIRM BATTLE: {puke(props)} </div>
+}
+
+ConfirmBattle.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  playersInfo: PropTypes.array.isRequired
+};
+module.exports = ConfirmBattle;
+var React = require('react');
 
 function ConfirmBattle (props) {
   return props.isLoading === true
     ? <p> LOADING! </p>
-    : <p> CONFIRM BATTLE !</p>
+    : <div>CONFIRM BATTLE: {puke(props)}</div> 
 }
 
 module.exports = ConfirmBattle;
